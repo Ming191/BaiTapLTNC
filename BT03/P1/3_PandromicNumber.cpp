f7035f8386246097b773fa795cb4afaf81728a30
Nguyễn Minh <138985110+Ming191@users.noreply.github.com>
2024-03-08 00:19:34 +0700

    Update 3_PandromicNumber.cpp

--- BT03/P1/3_PandromicNumber.cpp
+++ BT03/P1/3_PandromicNumber.cpp
@@ -1,36 +1,36 @@
-#include<bits/stdc++.h>
+    #include<bits/stdc++.h>
 
-using namespace std;
+    using namespace std;
 
-bool check(long long n) {
-    long long res = 0;
-    while(res < n) {
-        res = res*10 + n%10;
-        n/=10;
-    }
-    return (res == n || res == n/10 );
-}
-
-int main() {
-    int q;
-    cin >> q;
-    while (q--)
-    {
-        int count = 0;
-        long long l,h;
-        cin >> l >> h;
-
-        while (l <= h)
-        {
-            if(check(l)) count++;
-            l++;
+    bool check(long long n) {
+        long long res = 0;
+        while(res < n) {
+            res = res*10 + n%10;
+            n/=10;
         }
+        return (res == n || res == n/10 );
+    }
 
-        cout << count << endl;
+    int main() {
+        int q;
+        cin >> q;
+        while (q--)
+        {
+            int count = 0;
+            long long l,h;
+            cin >> l >> h;
+
+            while (l <= h)
+            {
+                if(check(l)) count++;
+                l++;
+            }
+
+            cout << count << endl;
+            
+        }
+        return 0;
         
     }
-    return 0;
-    
-}
 
-// 23132
+    // 23132
